6ebd1818c5bdfde335b757394bce9096702cf35f
Sabit Murzaliev <sabit.murzaliev@gmail.com>
2026-01-30 16:29:26 +0500

    add hello controller

---
+++ src/main/java/kz/sab1tm/satpa/controllers/TestController.java
@@ -0,0 +1,13 @@
+package kz.sab1tm.satpa.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String hello() {
+        return "hello!";
+    }
+}

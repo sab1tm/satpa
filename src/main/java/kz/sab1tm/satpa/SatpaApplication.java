ad75013a4f6ca7105950d69e5faf66ec0ce4ee10
Sabit Murzaliev <sabit.murzaliev@gmail.com>
2026-01-30 16:28:18 +0500

    init

---
+++ src/main/java/kz/sab1tm/satpa/SatpaApplication.java
@@ -0,0 +1,13 @@
+package kz.sab1tm.satpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/sab1tm/satpa/SatpaApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.sab1tm.satpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
